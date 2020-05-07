31d41209f2805cf298ec46c4c4b23f3e750a0666
Giulio Giovanella <giulio7771@gmail.com>
2020-05-06 21:49:25 -0300

    mapa funcionando com marcação

--- a6/App.js
+++ a6/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+import MapView from 'react-native-maps';
+
+export default class App extends Component{
+  state = {
+    latitude: -26.905788,
+    longitude: -49.079369,
+  };
+
+  render() {
+    const { latitude, longitude } = this.state;
+    return (
+      <MapView 
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.0042,
+          longitudeDelta: 0.0031  
+        }}
+        style={styles.mapView}
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude,
+            longitude
+          }}
+        ></MapView.Marker>
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mapView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+
   },
 });

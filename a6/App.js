f8c2996ba41fa0391229b173cfff3be2112514cc
Giulio Giovanella <giulio7771@gmail.com>
2020-05-17 00:24:25 -0300

    pegando a posição do gps

--- a6/App.js
+++ a6/App.js
@@ -9,6 +9,24 @@ export default class App extends Component{
     longitude: -49.079369,
   };
 
+  componentDidMount() {
+    const geoOptions = {
+      enableHightAccuracy: true,
+      timeOut: 20000,
+      maximumAge: 60*60*24
+    };
+
+    navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoFailure, geoOptions);
+  }
+  geoSuccess = (position) => {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }
+  geoFailure = (err) => {
+    console.log("geo failure");
+  }
   render() {
     const { latitude, longitude } = this.state;
     return (

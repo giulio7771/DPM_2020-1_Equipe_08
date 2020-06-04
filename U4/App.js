cec8f848f66103e2bba6322bdf3998f45577c743
Giulio Giovanella <giulio7771@gmail.com>
2020-06-03 22:36:05 -0300

    tela inicio almost

--- U4/App.js
+++ U4/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+
+import MapView from 'react-native-maps';
+
+export default class App extends Component{
+  state = {
+    name: "calouro(a)",
+    onChangeText: () => console.log("asd"),
+  };
+
+  setName(name) {
+    this.setState({
+      ...this.state,
+      name
+    });
+  }
+
+  buttonPress() {
+
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Bem vindo a Furb, {name}!</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome/apelido"
+          onChangeText={(val) => this.setName(val)}
+        />
+        <Button style={styles.button} title="Go" onPress={this.buttonPress} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +44,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    width: 200,
+    margin: 5,
+  },
+  button: {
+    margin: 5,
+    width: 200,
+  }
 });
